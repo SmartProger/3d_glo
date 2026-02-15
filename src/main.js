a24cf57aa4701c4ea2b7148bca73ab503775e1ae
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-02-15 11:55:42 +0300

    урок 20 практика выполнена

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import "./css/bootstrap.min.css";
 import "./css/style.min.css";
 
 import {timer} from "./modules/timer";
+import {menu} from "./modules/menu";
+import {modal} from "./modules/modal";
 
 timer("16 february 2026");
+menu();
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,15 @@
+export const modal = () => {
+  const buttons = document.querySelectorAll(".popup-btn");
+  const modal = document.querySelector(".popup");
+  const closeBtn = modal.querySelector(".popup-close");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+};
