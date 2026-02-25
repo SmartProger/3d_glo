a5c23ffdb6480b719a68818a23aa2a6ff930f8dc
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-02-25 23:41:01 +0300

    27 урок практика выполнена

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import {regExpForms} from "./modules/regExpForms";
 import {tabs} from "./modules/tabs";
 import {slider} from "./modules/slider";
 import {calc} from "./modules/calc";
+import {sendForm} from "./modules/sendForm";
 
 timer("19 february 2026");
 menu();
@@ -18,3 +19,12 @@ regExpForms();
 tabs();
 slider();
 calc(100);
+sendForm({
+  formId: "form1",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
